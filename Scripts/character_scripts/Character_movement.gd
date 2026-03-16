a203b2b492f90b3a61276e554b20cd843ed4d393
Taco ( Andrew ) <andrewaravenamunoz@gmail.com>
2026-03-17 10:19:31 +1300

    added character movement.

--- Scripts/character_scripts/Character_movement.gd
+++ Scripts/character_scripts/Character_movement.gd
@@ -4,10 +4,19 @@ extends CharacterBody3D
 @onready var _camera_3d: Camera3D = $CameraPivot/SpringArm3D/Camera3D
 
 
+
 @export_range(0.0, 1.0) var mouse_sensitivity = 0.01
 @export var tilt_limit = deg_to_rad(75)
+
+@export_group("movement")
+@export var move_speed := 10.0
+@export var acceleration := 20.0
 const FRICTION = 10.0
 const VELOCITY = 100.0
+
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -18,14 +27,21 @@ func _unhandled_input(event: InputEvent) -> void:
 		_camera_pivot.rotation.x = clampf(_camera_pivot.rotation.x, -tilt_limit, tilt_limit)
 		_camera_pivot.rotation.y += -event.screen_relative.x * mouse_sensitivity
 
+
 func _physics_process(delta: float) -> void:
 	
 	var input_vec = Input.get_vector("aeft","dight","walk","soundwards")
-	if input_vec != Vector2.ZERO:
-		var forward = _camera_pivot.transform.basis.z
-		var right = _camera_pivot.transform.basis.x
-		var move_dir = (right * input_vec.x) + (forward * input_vec.y)
-		velocity = move_dir * delta *1000
-	else:
-		velocity = lerp(velocity, Vector3.ZERO, delta *10)
+	var forward = _camera_pivot.transform.basis.z
+	var right = _camera_pivot.transform.basis.x
+	var move_dir = (right * input_vec.x) + (forward * input_vec.y)
+	move_dir.y = 0.0
+	move_dir = move_dir.normalized()
+	velocity = velocity.move_toward(move_dir * move_speed , acceleration * delta)
+	
 	move_and_slide()
+
+
+		
+		
+	
+		
